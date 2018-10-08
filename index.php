ecd56be3249e5e29adbc330acbe81943f9a48e6c
hackerscrackers <41089042+hackerscrackers@users.noreply.github.com>
2018-10-08 16:22:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
 <?php 
-echo "The World Is Your! - Scarface \n";
+echo "The World Is Your! - Scarface" . "\n\n\n";
 
-$quotes = array("no, provare no. Fare o non fare. Non c’è provare (Yoda in Star Wars - Il ritorno dello Jedi)", "Perché ci tieni così tanto a entrare nel gruppo, visto che sei fatta per emergere. (Ian Wallace in Una ragazza e il suo sogno)", "La vita scappa via in fretta. Se uno non si ferma e non si guarda attorno, rischia di sprecarla. (Ferris in Una pazza giornata di vacanza)", "Molti di quelli che vivono meritano la morte, e molti di quelli che muoiono meritano la vita. Tu sei in grado di valutare, Frodo? Non essere troppo ansioso di elargire morte e giudizi. Anche i più saggi non conoscono tutti gli esiti. (Gandalf in Il Signore degli Anelli - La compagnia dell’anello)", "Oh, sì. Il passato può fare male, ma a mio modo di vedere dal passato puoi scappare oppure imparare qualcosa. (Rafiki ne Il Re Leone)", "Non si vive per accontentare gli altri. La scelta deve essere solo tua. Perché quando andrai ad affrontare quella creatura, ci dovrai andare da sola. (La Regina Bianca in Alice in Wonderland)", "Io voglio che voi invitiate il “sì” nella vostra vita, perché il “sì”, a sua volta, vi risponderà sì! Quando voi dite sì, entrate nella sfera del possibile!” (Terrence Bundley in Yes Man)", "Noi siamo quello che scegliamo di essere. Ora scegli! (Goblin in Spiderman)", "Sono salito sulla cattedra per ricordare a me stesso che dobbiamo sempre guardare le cose da angolazioni diverse. E il mondo appare diverso da quassù. Non vi ho convinti? Venite a vedere voi stessi. Coraggio! È proprio quando credete di sapere qualcosa che dovete guardarla da un'altra prospettiva (John Keating ne L'attimo fuggente)");
-  
+$quotes = array("no, provare no. Fare o non fare. Non c’è provare (Yoda in Star Wars - Il ritorno dello Jedi)",
+                "Perché ci tieni così tanto a entrare nel gruppo, visto che sei fatta per emergere. (Ian Wallace in Una ragazza e il suo sogno)",
+                "La vita scappa via in fretta. Se uno non si ferma e non si guarda attorno, rischia di sprecarla. (Ferris in Una pazza giornata di vacanza)",
+                "Molti di quelli che vivono meritano la morte, e molti di quelli che muoiono meritano la vita. Tu sei in grado di valutare, Frodo? Non essere troppo ansioso di elargire morte e giudizi. Anche i più saggi non conoscono tutti gli esiti. (Gandalf in Il Signore degli Anelli - La compagnia dell’anello)",
+                "Oh, sì. Il passato può fare male, ma a mio modo di vedere dal passato puoi scappare oppure imparare qualcosa. (Rafiki ne Il Re Leone)",
+                "Non si vive per accontentare gli altri. La scelta deve essere solo tua. Perché quando andrai ad affrontare quella creatura, ci dovrai andare da sola. (La Regina Bianca in Alice in Wonderland)",
+                "Io voglio che voi invitiate il “sì” nella vostra vita, perché il “sì”, a sua volta, vi risponderà sì! Quando voi dite sì, entrate nella sfera del possibile!” (Terrence Bundley in Yes Man)",
+                "Noi siamo quello che scegliamo di essere. Ora scegli! (Goblin in Spiderman)",
+                "Sono salito sulla cattedra per ricordare a me stesso che dobbiamo sempre guardare le cose da angolazioni diverse. E il mondo appare diverso da quassù. Non vi ho convinti? Venite a vedere voi stessi. Coraggio! È proprio quando credete di sapere qualcosa che dovete guardarla da un'altra prospettiva (John Keating ne L'attimo fuggente)",
+                "Smettila di pensare a quello che vogliono gli altri. Non pensare a quello che voglio io, a quello che vuole lui o a quello che vogliono i tuoi. Tu che cosa vuoi? (Noah in Le pagine della nostra vita)",
+                "Se hai intenzione di provare, vai fino in fondo. Altrimenti non cominciare neanche. Potrebbe voler dire perdere la ragazza, la moglie, i parenti, il lavoro, e forse anche la testa.
+                  Potrebbe voler dire non mangiare per tre, quattro giorni. Potrebbe voler dire gelare su una panchina del parco, potrebbe voler dire la prigione, potrebbe voler dire la derisione, lo scherno, l'isolamento. L'isolamento è il premio.
+                  Tutto il resto è un test di resistenza, per vedere fino a che punto sei veramente disposto a farlo. E tu lo farai. Nonostante i rifiuti e le peggiori probabilità di successo, e sarà meglio di qualunque cosa tu possa immaginare...
+                  Se hai intenzione di provare, vai fino in fondo. Non c'è una sensazione al pari di questa. Sarai da solo con gli Dei, e il fuoco incendierà le tue notti. Cavalcherai la tua vita dritto verso una risata perfetta. È l'unica battaglia buona che ci sia. (Chinaski in Factotum)
+                  Dopotutto, domani è un altro giorno. (Rossella O'Hara in Via col vento)",
+                "La vita è avere coraggio e affrontare l'ignoto. Trovarsi l'un l'altro e sentirsi. È questo lo scopo della vita. (da I sogni segreti di Walter Mitty)",
+                "Non sono le nostre capacità che dimostrano chi siamo davvero, sono le nostre scelte. (Silente in Harry Potter e la Camera dei segreti)",
+                "La nostra vita è fatta di occasioni, anche di quelle che abbiamo perso. (da Il curioso caso di Benjamin Button)",
+                "La felicità è reale solo se viene condivisa. (da Into the wild – Nelle terre selvagge)",
+                "Quel giorno, non so proprio perché, decisi di andare a correre un po', perciò corsi fino alla fine della strada, e una volta lì pensai di correre fino la fine della città, e una volta lì pensai di correre attraverso la contea di Greenbow.
+                  Poi mi dissi: visto che sono arrivato fino a qui tanto vale correre attraverso il bellissimo stato dell' Alabama. E cosi feci. Corsi attraverso tutta l'Alabama, e non so perché continuai ad andare.
+                  Corsi fino all'oceano e, una volta lì mi dissi: visto che sono arrivato fino a qui tanto vale girarmi e continuare a correre. Quando arrivai a un altro oceano, mi dissi: visto che sono arrivato fino a qui, tanto vale girarmi di nuovo e continuare a correre. (da Forrest Gump)",       
+                "Hey! Non permettere mai a nessuno di dirti che non sai fare qualcosa. Neanche a me. Ok? Se hai un sogno tu lo devi proteggere. Quando le persone non sanno fare qualcosa lo dicono a te che non la sai fare. Se vuoi qualcosa, vai e inseguila. Punto. (Chris in La ricerca della felicità)",
+                "Non pensare mai di non meritare ciò che vuoi! (Patrick in Dieci cose che odio di te)",
+                "Dopo un po’ impari a ignorare il nome che ti danno e ti fidi solo di chi sei! Ognuno di voi è ostacolo di se stesso… (Shrek terzo)",
+                "Vuoi dire che Dio è vanitoso? No, non è vanitoso. Vuole godersi le cose belle con noi. Io credo che Dio si incazza se tu, di fronte al colore viola di un campo di fiori, neanche te ne accorgi. (Celie e Shug Avery ne Il colore Viola)",
+                "No, forse non vincerò. Forse l’unica cosa che potrò fare sarà quello di rendergli la vita difficile. Ma per battere me, dovrà riuscire ad uccidermi, e per uccidermi dovrà avere il fegato di stare di fronte a me. E per fare questo, dovrà essere pronto a morire anche lui. E io non so se sia disposto a farlo… non lo so. (Rocky IV)",
+                "Io credo ancora nel Paradiso, ma almeno adesso so che non è un posto da cercare fuori. Perché non è dove vai, lo trovi dentro, quando senti per un momento di far parte di qualcosa... e se lo trovi, quel momento, dura per sempre (Richard in The Beach)",
+                "Certi hanno una luce attorno che illumina anche le altre persone. Penso che forse alcuni di loro stavano in un tunnel e in quel tunnel forse l’unica luce che avevano stava dentro di loro. E poi, anche tanto tempo dopo che sono usciti dal tunnel, continuano a splendere per tutti gli altri. (Precious)",
+                "Perché voler credere in tutto allo stesso momento corrisponde esattamente a non credere in niente (Padre di Pi in Vita di Pi)"
+);
 echo $quotes[array_rand($quotes)] . "\n";
 
 ?>
